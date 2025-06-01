a37658f6ce7a0e5d2b3f14ce996a65dc1412f6a5
Amr Tamer Elharery <amrelharery@gmail.com>
2025-06-01 19:24:01 +0300

    Add localization route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocalizationController;
+
+Route::get('locale/{locale}', [LocalizationController::class, 'setLocale']);
 
 Route::get('/', function () {
     return view('welcome');
