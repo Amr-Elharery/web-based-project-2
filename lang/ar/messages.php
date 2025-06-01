2cc366274ca81282d465aefa133dd13d7539cb90
Amr Tamer Elharery <amrelharery@gmail.com>
2025-06-01 19:13:36 +0300

    Add Arabic translation file

---
+++ lang/ar/messages.php
@@ -0,0 +1,24 @@
+<?php 
+  return [
+    'welcome' => 'مرحبا بك في موقعنا',
+    'create_account' => 'إنشاء حساب جديد',
+    'email' => 'البريد الإلكتروني',
+    'enter_email' => 'أدخل بريدك الإلكتروني',
+    'enter_password' => 'أدخل كلمة المرور',
+    'password' => 'كلمة المرور',
+    'confirm_password' => 'تأكيد كلمة المرور',
+    'username' => 'اسم المستخدم',
+    'enter_username' => 'أدخل اسم المستخدم',
+    'full_name' => 'الاسم الكامل',
+    'enter_full_name' => 'أدخل اسمك الكامل',
+    'address' => 'العنوان',
+    'enter_address' => 'أدخل عنوانك',
+    'phone_number' => 'رقم الهاتف',
+    'enter_phone_number' => 'أدخل رقم هاتفك',
+    'whatsapp_number' => 'رقم الواتساب',
+    'enter_whatsapp_number' => 'أدخل رقم الواتساب الخاص بك',
+    'profile_picture' => 'صورة الملف الشخصي',
+    'upload_profile_picture' => 'تحميل صورة الملف الشخصي',
+    'submit' => 'إرسال',
+  ]
+?>
