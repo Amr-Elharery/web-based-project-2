553bb51fb0e14b658b4d2af5a6ff497cc14fe823
Mona Ashraf <monaashraf5946@gmail.com>
2025-06-10 16:14:00 +0300

    Create Services.php

---
+++ config/Services.php
@@ -0,0 +1,7 @@
+return [
+    
+    'whatsapp_validator' => [
+        'key' => 'your_actual_api_key_here', // Direct value
+        'host' => 'whatsapp-numbervalidator3.p.rapidapi.com', // Direct value
+    ],
+];
