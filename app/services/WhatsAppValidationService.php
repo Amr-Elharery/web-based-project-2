37b98be41b2e95cebfb0e7e53f72a59d6b0f7fb6
Mona Ashraf <monaashraf5946@gmail.com>
2025-06-10 16:20:17 +0300

    Update WhatsAppValidationService.php

--- app/services/WhatsAppValidationService.php
+++ app/services/WhatsAppValidationService.php
@@ -14,8 +14,8 @@ class WhatsAppValidationService
     public function __construct()
     {
         $this->client = new Client();
-        $this->apiKey = 'your_actual_api_key_here'; 
-        $this->host = 'whatsapp-numbervalidator3.p.rapidapi.com'; 
+        $this->apiKey = config('services.whatsapp_validator.key');
+        $this->host = config('services.whatsapp_validator.host');
     }
 
     public function validateNumber($phoneNumber)
